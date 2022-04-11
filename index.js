c83f25353d206d4cbc395adea7f4d65fa614071e
Julie <jatkin80@ford.com>
2022-04-11 16:50:26 -0600

    complete exercise

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function boom(){
-  document.querySelector("#message").textContent = "BOOM"
+function boom() {
+    document.querySelector("#message").textContent = "BOOM"
 }
+
+const button = document.querySelector('button')
+button.addEventListener("click", event => {
+    boom()
+})
